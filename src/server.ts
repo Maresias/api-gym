716a9da6dd4487bac3cd3b2a04fabf7a9c3b5b77
Maresias <ale.maresias@gmail.com>
2024-10-30 23:07:28 -0300

    iniciando routes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,27 @@
 import { app } from './app'
 import { env } from './env'
+import { prisma } from './lib/prisma'
+import { z } from 'zod'
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
 
 app
   .listen({
