3cd4aae3447156e3cda8fc3579e02100794a3f1a
Maresias <ale.maresias@gmail.com>
2024-11-01 01:06:16 -0300

    controlle registro

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,8 @@
 import { app } from './app'
 import { env } from './env'
-import { prisma } from './lib/prisma'
-import { z } from 'zod'
+import { appRegister } from './http/routes'
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
-
+app.register(appRegister)
 app
   .listen({
     host: '0.0.0.0',
