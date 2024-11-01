8cb2a27107c8fcb2b16d252c85bbb10f5f2310ca
Maresias <ale.maresias@gmail.com>
2024-11-01 02:37:36 -0300

    Repository Pattern

--- src/use-case/register.ts
+++ src/use-case/register.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
+import { PrismaUserRepository } from '@/repositories/prisma-user-repository'
 
 interface registerUseCaseRequest {
   name: string
@@ -24,11 +25,11 @@ export async function registerUseCaseRequest({
 
   const password_hash = await hash(password, 6)
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
+  const prismaUserRepository = new PrismaUserRepository()
+
+  await prismaUserRepository.create({
+    name,
+    email,
+    password_hash,
   })
 }
