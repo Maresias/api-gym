10e20ba2787b61d257e5c1913d06abc014d9fd68
Maresias <ale.maresias@gmail.com>
2024-11-01 22:49:30 -0300

    handler de erros global

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
 import { fastify } from 'fastify'
 import { appRegister } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 app.register(appRegister)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
