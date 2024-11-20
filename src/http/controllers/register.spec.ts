daa06f0feaf0d95b0929d3609db906d024fd056f
Maresias <ale.maresias@gmail.com>
2024-11-19 22:19:43 -0300

    test e2e register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,22 @@
-import { test } from 'vitest'
+import { describe, it, expect, beforeEach, afterEach } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Test e2e Register', () => {
+  beforeEach(async () => {
+    app.ready()
+  })
 
-test('kk', () => {})
+  afterEach(async () => {
+    app.close()
+  })
+  it('should be possible register user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@gmail.com',
+      password: '1234567',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
