b441eb6e5e86155d298fdc2fd35510d2392b09bc
Maresias <ale.maresias@gmail.com>
2024-11-24 22:08:26 -0300

    Test e2e create gym

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -1,5 +1,31 @@
-import { describe, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createUserAuthenticate } from '@/utils/test/create-user-authenticate.spec'
 
 describe('Test ( e2e ) gyms', () => {
-  it('should be able possible create gym', async () => {})
+  beforeEach(() => {
+    app.ready()
+  })
+
+  afterEach(() => {
+    app.close()
+  })
+  it('should be able possible create gym', async () => {
+    const userAuth = await createUserAuthenticate(app)
+
+    const { token } = userAuth
+    const gym = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript',
+        description: 'Treino pesado',
+        phone: '0000-00000',
+        latitude: -23.786952,
+        longitude: -45.556697,
+      })
+
+    expect(gym.statusCode).toEqual(201)
+  })
 })
